4eca1c461325c81c910b815e167c743313aa809b
Michał Budzyński <michal@virtualdesign.pl>
2016-10-02 06:03:00 +0100

    :sushi: hw example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::net::{TcpListener, TcpStream};
+use std::io::{Read, Write};
+use std::thread;
+
+static HOST: &'static str = "127.0.0.1:8080";
+
+fn handle_client(mut stream: TcpStream) {
+    let response = b"HTTP/1.1 200 OK\r\n\r\nHello world";
+    match stream.write(response) {
+        Ok(_) => println!("OK"),
+        Err(e) => println!("Fail: {}", e),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind(HOST).unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                thread::spawn(move|| {
+                    handle_client(stream)
+                });
+            }
+            Err(e) => println!("Unable to read stream: {}", e),
+        }
+    }
+    drop(listener);
 }
