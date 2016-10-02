3394724cfc9c20066bb2c482402dae447ae033f9
Michał Budzyński <michal@virtualdesign.pl>
2016-10-02 06:18:00 +0100

    :earth_americas: send headers

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,19 @@ use std::thread;
 static HOST: &'static str = "127.0.0.1:8080";
 
 fn handle_client(mut stream: TcpStream) {
-    let response = b"HTTP/1.1 200 OK\r\n\r\nHello world";
-    match stream.write(response) {
+    let headers = b"HTTP/1.1 200 OK\r\n\r\n";
+    let content = b"Hello world";
+
+    match stream.write(headers) {
         Ok(_) => println!("OK"),
         Err(e) => println!("Fail: {}", e),
     }
+
+    match stream.write(content) {
+        Ok(_) => println!("OK"),
+        Err(e) => println!("Fail: {}", e),
+    }
+
 }
 
 fn main() {
